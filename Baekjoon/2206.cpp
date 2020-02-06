104c14ad69c052bbc077d58a48b8214d775557ea
Park Changwan <pcw109550@gmail.com>
2020-02-07 02:13:07 +0900

    [Baekjoon] BFS/DFS

--- Baekjoon/2206.cpp
+++ Baekjoon/2206.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
+int input[1000][1000];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-void debug(int* input) {
+void debug(int *state) {
     cout << "\n";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cout << *(input + m * i + j) << " ";
+            cout << *(state + m * i + j) << " ";
         }
         cout << "\n";
     }
@@ -16,11 +17,79 @@ void debug(int* input) {
 
 int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); int N, M;
-    cin >> N >> M; string s; char *c; getline(cin, s); n = N; m = M; int input[N][M] = {};
+    cin >> N >> M; string s; char *c; getline(cin, s); n = N; m = M;
     queue<pair<int, int>> wall;
     for (int i = 0; i < N; i++) {
         getline(cin, s); c = (char *)s.c_str();
         for (int j = 0; j < M; j++) { input[i][j] = s[j] == '1'; }
     }
-    // FAILED
+
+    bool visited1[N][M] = {}; set<pair<int, int>> wall1;
+    int length[N][M] = {}; length[0][0] = 1;
+    queue<pair<int, int>> Q; Q.push({0, 0}); visited1[0][0] = true;
+    while (!Q.empty()) {
+        auto current = Q.front(); Q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int X = current.first + dx[dir], Y = current.second + dy[dir];
+            if (X < 0 || Y < 0 || X >= N || Y >= M) { continue; }
+            if (input[X][Y] == 1) { wall1.insert({X, Y}); continue; }
+            if (!visited1[X][Y]) {
+                visited1[X][Y] = true; length[X][Y] = length[current.first][current.second] + 1;
+                Q.push({X, Y}); continue;
+            }
+            if (length[X][Y] > length[current.first][current.second] + 1) {
+                length[X][Y] = length[current.first][current.second] + 1;
+                Q.push({X, Y}); continue;
+            }
+        }
+    }
+    int result = -1;
+    if (length[N - 1][M - 1] > 0) {
+        result = length[N - 1][M - 1];
+    }
+
+    bool visited2[N][M] = {}; set<pair<int, int>> wall2;
+    Q.push({N - 1, M - 1}); visited2[N - 1][M - 1] = true;
+    while (!Q.empty()) {
+        auto current = Q.front(); Q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int X = current.first + dx[dir], Y = current.second + dy[dir];
+            if (X < 0 || Y < 0 || X >= N || Y >= M) { continue; }
+            if (input[X][Y] == 1) { wall2.insert({X, Y}); continue; }
+            if (!visited2[X][Y]) {
+                visited2[X][Y] = true; Q.push({X, Y});
+            }
+        }
+    }
+
+    set<pair<int, int>> intersect;
+
+    set_intersection(wall1.begin(), wall1.end(), wall2.begin(), wall2.end(), inserter(intersect, intersect.begin()));
+    for (auto it = intersect.begin(); it != intersect.end(); it++) {
+        auto here = *it;
+        int length[N][M] = {}; bool visited[N][M] = {};
+        Q.push({0, 0}); visited[0][0] = true; length[0][0] = 1;
+        while (!Q.empty()) {
+            auto current = Q.front(); Q.pop();
+            for (int dir = 0; dir < 4; dir++) {
+                int X = current.first + dx[dir], Y = current.second + dy[dir];
+                if (X < 0 || Y < 0 || X >= N || Y >= M) { continue; }
+                if (!(X == here.first && Y == here.second) && input[X][Y] == 1) { continue; }
+                if (!visited[X][Y]) {
+                    visited[X][Y] = true; length[X][Y] = length[current.first][current.second] + 1;
+                    Q.push({X, Y}); continue;
+                }
+                if (length[X][Y] > length[current.first][current.second] + 1) {
+                    length[X][Y] = length[current.first][current.second] + 1;
+                    Q.push({X, Y}); continue;
+                }
+            }
+        }
+        //debug((int *)length);
+        if (length[N - 1][M - 1] > 0) {
+            if (result < length[N - 1][M - 1]) { result = length[N - 1][M - 1]; }
+        }
+    }
+    cout << result;
+    // GOT TIMEOUT :C
 }
--- Baekjoon/9466.cpp
+++ Baekjoon/9466.cpp
@@ -1,16 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(void) {
-    int T; cin >> T;
-    for (int i = 0; i < T; i++) {
-        int N; cin >> N; int input[N] = {}; bool resolved[N] = {};
-        int result = 0;
-        for (int j = 0; j < N; j++) {cin >> input[i]; input[i]--; }
-        for (int j = 0; j < N; j++) {
-            if (resolved[j]) { continue; }
-            stack<int> S; int start = j;
-            
+#define totalsize 400000
+int input[100000];
+int resolved[100000];
+int store[100000];
 
+enum state {NOTUSED, CYCLE, CHAIN, NOTCYCLE};
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int T, N, result = 0, size = 0; cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> N; memset(resolved, 0, totalsize); memset(store, 0, totalsize);
+        for (int j = 0; j < N; j++) { cin >> input[j]; input[j]--; }
+        for (int j = 0; j < N; j++) {
+            if (resolved[j] == CYCLE || resolved[j] == NOTCYCLE) { continue; }
+            int idx = j; bool cycle = false;
+            while (!cycle) {
+                if (resolved[idx] == CHAIN) {
+                    resolved[idx] = CYCLE;
+                    for (int k = 0; k < size; k++) {
+                        if (store[k] == idx) {
+                            resolved[store[k]] = CYCLE;
+                            cycle = true; continue;
+                        }
+                        resolved[store[k]] = cycle ? CYCLE : NOTCYCLE;
+                    }
+                    size = 0;
+                    if (cycle) { break; }
+                }
+                else if (resolved[idx] == NOTUSED) {
+                    resolved[idx] = CHAIN; store[size++] = idx; 
+                    idx = input[idx];
+                }
+                else if (resolved[idx] == NOTCYCLE || resolved[idx] == CYCLE) {
+                    for (int k = 0; k < size; k++) {
+                        resolved[store[k]] = NOTCYCLE;
+                    }
+                    size = 0;
+                    break;
+                }
+            }
         }
+        result = 0;
+        for (int j = 0; j < N; j++) {
+            result += resolved[j] != CYCLE;
+        }
+        cout << result << '\n';
     }
 }
